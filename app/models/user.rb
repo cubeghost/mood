e49ddbd4cb96df66cdf94967f033681b97001e26
goosey <hello@goose.im>
2015-02-09 16:28:36 -0500

    make it into a rails app
    it's rails now

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+
+    before_save { self.email = email.downcase }
+    validates :username,  presence: true, length: { maximum: 24 }, uniqueness: { case_sensitive: false }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, length: { minimum: 8 }
+
+end
