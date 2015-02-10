dc696e118c54c09d0439bc40cb82f90aa2620616
goosey <hello@goose.im>
2015-02-10 16:54:48 -0500

    fix db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.create!(username:  "alex",
+             email:     "hello@goose.im",
+             password:              "foobar22",
+             password_confirmation: "foobar22")
+
 user = User.find(1)
 content = '{"feels":{"mood":"4","energy":"3","pain":"5","fog":"6"},"notes":"imitrex","time":1422038001715}'
 user.entries.create!(content: content)
